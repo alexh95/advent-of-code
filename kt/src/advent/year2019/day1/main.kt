8d30b549de98b4d45b39ba6907ed365e5b088e6c
alexh95 <alexhuruiala@gmail.com>
2019-12-01 18:56:49 +0200

    day 1 part two

--- kt/src/advent/year2019/day1/main.kt
+++ kt/src/advent/year2019/day1/main.kt
@@ -3,6 +3,18 @@ package advent.year2019.day1
 import java.io.File
 
 fun main() {
-    val fuelSum = File("src/advent/year2019/day1/input.txt").readLines().map{it.toInt() / 3 - 2}.sum()
-    println(fuelSum)
+    var modulesFuelSum = 0
+    var totalFuelSum = 0
+    File("src/advent/year2019/day1/input.txt").forEachLine {
+        var fuel = it.toInt() / 3 - 2
+        modulesFuelSum += fuel
+        totalFuelSum += fuel
+        while (fuel > 0) {
+            fuel = fuel / 3 - 2
+            if (fuel > 0) {
+                totalFuelSum += fuel;
+            }
+        }
+    }
+    File("src/advent/year2019/day1/output.txt").writeText("$modulesFuelSum\n$totalFuelSum")
 }
