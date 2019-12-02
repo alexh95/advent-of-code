81dc7f0aac46c195ee783ac51bb431a9719b059b
alexh95 <alexhuruiala@gmail.com>
2019-12-02 15:59:58 +0200

    day 2 part two

--- kt/src/advent/year2019/day2/main.kt
+++ kt/src/advent/year2019/day2/main.kt
@@ -2,23 +2,49 @@ package advent.year2019.day2
 
 import java.io.File
 
-fun main() {
-    val program = File("src/advent/year2019/day2/input.txt").readText().trim().split(",").map{it.toInt()}.toMutableList()
-    program[1] = 12;
-    program[2] = 2;
+fun calculate(program: MutableList<Int>) {
     var notFinished = true
-    var instructionIndex = 0
+    var instructionPointer = 0
     while (notFinished) {
-        val opcode: Int = program[instructionIndex]
+        val opcode: Int = program[instructionPointer]
         when (opcode) {
-            1 -> program[program[instructionIndex + 3]] = program[program[instructionIndex + 1]] + program[program[instructionIndex + 2]]
-            2 -> program[program[instructionIndex + 3]] = program[program[instructionIndex + 1]] * program[program[instructionIndex + 2]]
+            1 -> program[program[instructionPointer + 3]] = program[program[instructionPointer + 1]] + program[program[instructionPointer + 2]]
+            2 -> program[program[instructionPointer + 3]] = program[program[instructionPointer + 1]] * program[program[instructionPointer + 2]]
             99 -> notFinished = false
         }
-        instructionIndex += 4
-        if (instructionIndex >= program.size - 3) {
+        instructionPointer += 4
+        if (instructionPointer >= program.size - 3) {
             notFinished = false
         }
     }
-    File("src/advent/year2019/day2/output.txt").writeText(program[0].toString())
+}
+
+fun main() {
+    val originalProgram = File("src/advent/year2019/day2/input.txt").readText().trim().split(",").map{it.toInt()}
+    val program = originalProgram.toMutableList()
+    program[1] = 12
+    program[2] = 2
+    calculate(program)
+
+    var noun = 0
+    var verb = 0
+    var notFinished = true
+    while (notFinished) {
+        val newProgram = originalProgram.toMutableList()
+        newProgram[1] = noun
+        newProgram[2] = verb
+        calculate(newProgram)
+        if (newProgram[0] == 19690720) {
+            notFinished = false
+        } else {
+            ++noun
+            if (noun > 99) {
+                noun = 0
+                ++verb
+            }
+        }
+    }
+
+    val output = 100 * noun + verb
+    File("src/advent/year2019/day2/output.txt").writeText(program[0].toString() + "\n" + output.toString())
 }
