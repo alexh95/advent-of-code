d014e3795c45bffb532e0f557a091ac103e53de4
alexh95 <alexhuruiala@gmail.com>
2019-12-12 17:19:16 +0200

    day 12 part two

--- kt/src/advent/year2019/day12/main.kt
+++ kt/src/advent/year2019/day12/main.kt
@@ -4,7 +4,7 @@ import java.io.File
 import kotlin.math.abs
 import kotlin.test.assertEquals
 
-class V3i(val x: Int, val y: Int, val z: Int) {
+data class V3i(val x: Int, val y: Int, val z: Int) {
     constructor(array: IntArray) : this(array[0], array[1], array[2])
     constructor() : this(0, 0, 0)
     operator fun plus(v: V3i): V3i = V3i(x + v.x, y + v.y, z + v.z)
@@ -14,38 +14,82 @@ class V3i(val x: Int, val y: Int, val z: Int) {
 
 fun List<V3i>.sum(): V3i = reduceRight { v, acc -> v + acc }
 
-class Moon(val p: V3i, val dp: V3i) {
+data class Moon(val p: V3i, val dp: V3i) {
     constructor(p: V3i) : this(p, V3i())
-
+    fun update(moons: List<Moon>): Moon {
+        val newDP = dp + moons.map { m -> V3i(sign(m.p.x - p.x), sign(m.p.y - p.y), sign(m.p.z - p.z)) }.sum()
+        val newP = p + newDP
+        return Moon(newP, newDP)
+    }
     override fun toString(): String = "p=$p, dp=$dp"
 }
 
 fun sign(n: Int): Int = if (n > 0) 1 else if (n == 0) 0 else -1
 
+fun getMoons(input: List<String>): List<Moon> = input.map { V3i(it.substring(1, it.length - 1).split(", ").map { s -> s.substringAfter('=').toInt() }.toIntArray()) }.map { Moon(it) }
+
 fun totalEnergy(input: List<String>, steps: Int): Int {
-    val moonPositions = input.map { V3i(it.substring(1, it.length - 1).split(", ").map { s -> s.substringAfter('=').toInt() }.toIntArray()) }
-    var moons = moonPositions.map { Moon(it) }
+    var moons = getMoons(input)
     repeat(steps) {
-        moons = moons.map {moon ->
-            val newDP = moon.dp + moons.map { m -> V3i(sign(m.p.x - moon.p.x), sign(m.p.y - moon.p.y), sign(m.p.z - moon.p.z)) }.sum()
-            val newP = moon.p + newDP
-            Moon(newP, newDP)
-        }
+        moons = moons.map { it.update(moons) }
 //        println("After step ${it + 1}:\n${moons.joinToString("\n")}")
     }
     return moons.map { it.p.absum() * it.dp.absum() }.sum()
 }
 
+fun gcd(x: Long, y: Long): Long = if (y == 0L) x else gcd(y, x % y)
+
+fun lcm(x: Long, y: Long): Long = x * y / gcd(x, y)
+
+fun minStepsToPrevious(input: List<String>): Long {
+    var moons = getMoons(input)
+    val initialXValues = moons.map { listOf(it.p.x, it.dp.x) }
+    val initialYValues = moons.map { listOf(it.p.y, it.dp.y) }
+    val initialZValues = moons.map { listOf(it.p.z, it.dp.z) }
+    var xPeriod = -1L
+    var yPeriod = -1L
+    var zPeriod = -1L
+    var step: Long = 0L
+    var notFinished = true
+    while (notFinished) {
+        moons = moons.map { it.update(moons) }
+        ++step
+        val xValues = moons.map { listOf(it.p.x, it.dp.x) }
+        val yValues = moons.map { listOf(it.p.y, it.dp.y) }
+        val zValues = moons.map { listOf(it.p.z, it.dp.z) }
+
+        if (xPeriod == -1L && initialXValues == xValues) {
+            xPeriod = step
+        }
+        if (yPeriod == -1L && initialYValues == yValues) {
+            yPeriod = step
+        }
+        if (zPeriod == -1L && initialZValues == zValues) {
+            zPeriod = step
+        }
+
+        if (xPeriod > -1L && yPeriod > -1L && zPeriod > -1L) {
+            notFinished = false
+        }
+    }
+    return lcm(xPeriod, lcm(yPeriod, zPeriod))
+}
+
 fun main() {
-    val inputTest00 = listOf("<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>", "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>")
-    val energyTest00 = totalEnergy(inputTest00, 10)
-    assertEquals(179, energyTest00)
+    val inputTest0 = listOf("<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>", "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>")
+    val energyTest0 = totalEnergy(inputTest0, 10)
+    val minStepsTest0 = minStepsToPrevious(inputTest0)
+    assertEquals(179, energyTest0)
+    assertEquals(2772L, minStepsTest0)
 
-    val inputTest01 = listOf("<x=-8, y=-10, z=0>", "<x=5, y=5, z=10>", "<x=2, y=-7, z=3>", "<x=9, y=-8, z=-3>")
-    val energyTest01 = totalEnergy(inputTest01, 100)
-    assertEquals(1940, energyTest01)
+    val inputTest1 = listOf("<x=-8, y=-10, z=0>", "<x=5, y=5, z=10>", "<x=2, y=-7, z=3>", "<x=9, y=-8, z=-3>")
+    val energyTest1 = totalEnergy(inputTest1, 100)
+    val minStepsTest1 = minStepsToPrevious(inputTest1)
+    assertEquals(1940, energyTest1)
+    assertEquals(4686774924L, minStepsTest1)
 
-    val inputMain0 = File("src/advent/year2019/day12/input.txt").readLines()
-    val energyMain0 = totalEnergy(inputMain0, 1000)
-    println("$energyMain0\n")
+    val input = File("src/advent/year2019/day12/input.txt").readLines()
+    val energy = totalEnergy(input, 1000)
+    val minSteps = minStepsToPrevious(input)
+    println("$energy\n$minSteps\n")
 }
