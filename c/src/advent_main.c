0bf05140c026153f065fd4f7c648eed248e4fc00
alexh95 <alexhuruiala@gmail.com>
2023-12-06 02:02:57 +0200

    day 1 part 2 plus some improvements

--- c/src/advent_main.c
+++ c/src/advent_main.c
@@ -3,44 +3,46 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main()
+typedef struct buffer_
 {
-    int InputFile = open("../data/2023/day1/input.txt", O_RDONLY);
+    char *Data;
+    int Size;
+} buffer;
+
+buffer ReadFile(char *FileName)
+{
+    buffer Result = {};
+    int InputFile = open(FileName, O_RDONLY);
     if (InputFile < 0 )
     {
         printf("Cannot open file\n");
         exit(-1);
     }
-    int InputFileSize = lseek(InputFile, 0, SEEK_END);
-    char *InputData = (char *)malloc(InputFileSize);
+    Result.Size = lseek(InputFile, 0, SEEK_END);
+    Result.Data = (char *)malloc(Result.Size);
     lseek(InputFile, 0, SEEK_SET);
-    int ReadBytes = read(InputFile, InputData, InputFileSize);
+    int ReadBytes = read(InputFile, Result.Data, Result.Size);
     if (ReadBytes < -1)
     {
         printf("Cannot read file\n");
         exit(-2);
     }
+    return Result;
+}
+
+int SumNumberLines(buffer Buffer)
+{
     int NumberFirst = -1;
     int NumberLast = 0;
     int NumberSum = 0;
-    int LineStart = 0;
-    int AlphaNumFirst = -1;
-    int AlphaNumLast = 0;
-    int AlphaNumSum = 0;
-    for (int DataIndex = 0; DataIndex < InputFileSize; ++DataIndex)
+    for (int DataIndex = 0; DataIndex < Buffer.Size; ++DataIndex)
     {
-        char Character = InputData[DataIndex];
+        char Character = Buffer.Data[DataIndex];
         if (Character == '\n')
         {
             NumberSum += 10 * NumberFirst + NumberLast;
             NumberFirst = -1;
             NumberLast = 0;
-
-            for (int Index = LineStart; Index <= DataIndex; ++Index)
-            {
-                
-            }
-            LineStart = DataIndex + 1;
         }
         if (Character >= '0' && Character <= '9')
         {
@@ -50,9 +52,109 @@ int main()
                 NumberFirst = NumberLast;
             }
         }
-        
     }
-    printf("%d", NumberSum);
+    return NumberSum;
+}
+
+int StringLength(char *String)
+{
+    int Result = 0;
+    if (String)
+    {
+        while (String[Result])
+        {
+            ++Result;
+        }
+    }
+    return Result;
+}
+
+int StringIndexOf(char *String, int Size, char *Contained, int ContainedSize)
+{
+    int MatchingStartIndex = -1;
+    int ContainedIndex = 0;
+    for (int Index = 0; Index < Size; ++Index)
+    {
+        char Character = String[Index];
+        char ContainedCharacter = Contained[ContainedIndex];
+        if (Character == ContainedCharacter)
+        {
+            if (MatchingStartIndex == -1)
+            {
+                MatchingStartIndex = Index;
+            }
+            ++ContainedIndex;
+            if (ContainedIndex == ContainedSize)
+            {
+                return MatchingStartIndex;
+            }
+        }
+        else
+        {
+            MatchingStartIndex = -1;
+            ContainedIndex = 0;
+        }
+    }
+    return -1;
+}
+
+char *AlphaNumbers[] =
+{
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+};
+
+#define ArrayCount(Array) ((sizeof(Array)) / (sizeof(Array[0])))
+
+buffer StringClone(buffer String)
+{
+    buffer Result;
+    Result.Data = (char *)malloc(String.Size);
+    Result.Size = String.Size;
+    for (int Index = 0; Index < String.Size; ++Index)
+    {
+        Result.Data[Index] = String.Data[Index];
+    }
+    return Result;
+}
+
+buffer ParseAndReplaceAlphaNumbers(buffer Buffer)
+{
+    buffer Result = StringClone(Buffer);
+    int LineStart = 0;
+    for (int DataIndex = 0; DataIndex < Buffer.Size; ++DataIndex)
+    {
+        char Character = Buffer.Data[DataIndex];
+        if (Character == '\n')
+        {
+            for (int AlphaNumberIndex = 0; AlphaNumberIndex < ArrayCount(AlphaNumbers); ++AlphaNumberIndex)
+            {
+                char *AlphaNumber = AlphaNumbers[AlphaNumberIndex];
+                int AlphaNumberSize = StringLength(AlphaNumber);
+                for (int LineIndex = LineStart; LineIndex < DataIndex + 1; ++LineIndex)
+                {
+                    char *Line = Buffer.Data + LineIndex;
+                    char *ResultLine = Result.Data + LineIndex;
+                    int AlphaNumberLocation = StringIndexOf(Line, DataIndex + 1 - LineIndex, AlphaNumber, AlphaNumberSize);
+                    if (AlphaNumberLocation > -1)
+                    {
+                        ResultLine[AlphaNumberLocation] = '0' + AlphaNumberIndex + 1;
+                    }   
+                }
+            }
+            
+            LineStart = DataIndex + 1;
+        }
+    }
+    return Result;
+}
+
+int main()
+{
+    buffer Buffer = ReadFile("../data/2023/day1/input.txt");
+    int NumberSum = SumNumberLines(Buffer);
+    buffer ReplacedBuffer = ParseAndReplaceAlphaNumbers(Buffer);
+    int AlphaNumberSum = SumNumberLines(ReplacedBuffer);
+    printf("%d %d\n", NumberSum, AlphaNumberSum);
      
     return 0;
 }
