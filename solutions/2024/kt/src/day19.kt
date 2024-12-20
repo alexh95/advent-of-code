eddf8ece1ea65f0cfb5b1d8eb7954a58f25c11ea
alexh95 <alexhuruiala@gmail.com>
2024-12-20 19:32:25 +0200

    feat(kt): day 19 fast

--- solutions/2024/kt/src/day19.kt
+++ solutions/2024/kt/src/day19.kt
@@ -1,50 +1,40 @@
 fun solveDay19(input: List<String>): Solution {
-//    val input = ("r, wr, b, g, bwu, rb, gb, br\n" +
-//            "\n" +
-//            "brwrr\n" +
-//            "bggr\n" +
-//            "gbbr\n" +
-//            "rrbgbr\n" +
-//            "ubwu\n" +
-//            "bwurrg\n" +
-//            "brgr\n" +
-//            "bbrgwb").split("\n")
     val towels = input[0].split(", ")
     val patterns = input.drop(2)
-    val possiblePatternCount = countPossiblePatterns(towels, patterns)
-    val allPossiblePatternCount = countAllPossiblePatterns(towels, patterns)
+    val patternSolutionsCount = countPossiblePatterns(towels, patterns)
+    val possiblePatternCount = patternSolutionsCount.count { it > 0 }.toLong()
+    val allPossiblePatternCount = patternSolutionsCount.sum()
     return Solution(possiblePatternCount, allPossiblePatternCount)
 }
 
-private fun countPossiblePatterns(towels: List<String>, patterns: List<String>): Int {
-    return patterns.count { isPossiblePattern(towels, it) }
+private fun countPossiblePatterns(towels: List<String>, patterns: List<String>): List<Long> {
+    return patterns.map { countPossiblePatterns(towels, it) }
 }
 
-private fun countAllPossiblePatterns(towels: List<String>, patterns: List<String>): Int {
-    return patterns.sumOf { countPossiblePatterns(towels, it) }
-}
-
-private fun isPossiblePattern(towels: List<String>, pattern: String): Boolean {
-    return countPossiblePatterns(towels, pattern) > 0
-}
-
-private fun countPossiblePatterns(towels: List<String>, pattern: String): Int {
+private fun countPossiblePatterns(towels: List<String>, pattern: String): Long {
+    val indexToCount = mutableMapOf<Int, Long>()
     val patternOffsetToAvailableTowels = pattern.indices.map { pattern.substring(it) }
         .map { subPattern -> towels.filter { towel -> subPattern.indexOf(towel) == 0 } }
-    return findMatchingTowel(patternOffsetToAvailableTowels, pattern, 0)
+    return findMatchingTowel(indexToCount, patternOffsetToAvailableTowels, pattern, 0)
 }
 
-private fun findMatchingTowel(patternOffsetToAvailableTowels: List<List<String>>, pattern: String, patternOffset: Int): Int {
-    var combinations = 0
+private fun findMatchingTowel(indexToCount: MutableMap<Int, Long>, patternOffsetToAvailableTowels: List<List<String>>, pattern: String, patternOffset: Int): Long {
+    if (indexToCount.contains(patternOffset)) {
+        return indexToCount[patternOffset]!!
+    }
+
+    var combinations = 0L
     val usefulTowels = patternOffsetToAvailableTowels[patternOffset]
     for (towel in usefulTowels) {
         if (pattern.indexOf(towel, patternOffset) == patternOffset) {
             if (towel.length == pattern.length - patternOffset) {
                 combinations += 1
             } else {
-                combinations += findMatchingTowel(patternOffsetToAvailableTowels, pattern, patternOffset + towel.length)
+                combinations += findMatchingTowel(indexToCount, patternOffsetToAvailableTowels, pattern, patternOffset + towel.length)
             }
         }
     }
+    indexToCount[patternOffset] = combinations
+
     return combinations
 }
